931b7823db6feb6f92949b9e213c48bf3654f63b
Chih-Ming Chen <changecandy@gmail.com>
2017-10-01 11:28:04 -0500

    remove unused params

--- cli/bpr.cpp
+++ cli/bpr.cpp
@@ -27,20 +27,16 @@ int main(int argc, char **argv){
         printf("\t\tSave the representation data\n");
         printf("\t-dimensions <int>\n");
         printf("\t\tDimension of vertex representation; default is 64\n");
-        printf("\t-negative_samples <int>\n");
-        printf("\t\tNumber of negative examples; default is 5\n");
         printf("\t-sample_times <int>\n");
         printf("\t\tNumber of training samples *Million; default is 10\n");
         printf("\t-threads <int>\n");
         printf("\t\tNumber of training threads; default is 1\n");
         printf("\t-alpha <float>\n");
         printf("\t\tInit learning rate; default is 0.025\n");
-        printf("\t-reg <float>\n");
-        printf("\t\tRegularization term; default is 0.01\n");
 
         printf("Usage:\n");
         printf("\n[BPR]\n");
-        printf("./bpr -train net.txt -save rep.txt -dimensions 64 -sample_times 10 -negative_samples 5 -alpha 0.025 -reg 0.01 -threads 1\n");
+        printf("./bpr -train net.txt -save rep.txt -dimensions 64 -sample_times 10 -alpha 0.025 -threads 1\n");
 
         return 0;
     }
@@ -52,10 +48,8 @@ int main(int argc, char **argv){
     if ((i = ArgPos((char *)"-train", argc, argv)) > 0) strcpy(network_file, argv[i + 1]);
     if ((i = ArgPos((char *)"-save", argc, argv)) > 0) strcpy(rep_file, argv[i + 1]);
     if ((i = ArgPos((char *)"-dimensions", argc, argv)) > 0) dimensions = atoi(argv[i + 1]);
-    if ((i = ArgPos((char *)"-negative_samples", argc, argv)) > 0) negative_samples = atoi(argv[i + 1]);
     if ((i = ArgPos((char *)"-sample_times", argc, argv)) > 0) sample_times = atoi(argv[i + 1]);
     if ((i = ArgPos((char *)"-alpha", argc, argv)) > 0) init_alpha = atof(argv[i + 1]);
-    if ((i = ArgPos((char *)"-reg", argc, argv)) > 0) reg = atof(argv[i + 1]);
     if ((i = ArgPos((char *)"-threads", argc, argv)) > 0) threads = atoi(argv[i + 1]);
     
     BPR *bpr;
