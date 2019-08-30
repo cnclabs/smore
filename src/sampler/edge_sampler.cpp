8416593b03532915ee68c6842d7e6dd17a867a5b
chihming <changecandy@gmail.com>
2019-08-30 23:48:00 +0800

    init

--- src/sampler/edge_sampler.cpp
+++ src/sampler/edge_sampler.cpp
@@ -1,19 +1,55 @@
-#include <vector>
-#include <cmath>
-#include "../util/random.h"
+#include "edge_sampler.h"
 
-class EdgeSampler {
-    /* Edge Sampler
-     * EdgeSampler performs edge-based sampling
-     */
-    public:
-        EdgeSampler();
-        EdgeSampler(std::vector<double>& distribution, const double power);
-
-        // variables
-        std::vector<long> alias;
-        std::vector<long> probability;
-
-        // operations
-        std::vector<long> sample_an_edge();
+EdgeSampler::EdgeSampler(FileGraph* file_graph) {
+    this->build_from_file_graph(file_graph);
 }
+
+void EdgeSampler::build_from_file_graph(FileGraph* file_graph) {
+    std::cout << "Build Sampler:" << std::endl;
+    long from_index, to_index, to_offset;
+    int branch_size, current_index=0;
+    double weight;
+    std::vector<double> edge_distribution, neg_distribution;
+    this->edge_size = file_graph->num_edge;
+    this->vertex_size = file_graph->index2vertex.size();
+
+    // build vertex alias
+    std::cout << "\tbuild alias method" << std::endl;
+    edge_distribution.resize(this->edge_size, 0.0);
+    neg_distribution.resize(this->vertex_size, 0.0);
+    for (from_index=0; from_index<this->vertex_size; from_index++)
+    {
+        branch_size = file_graph->adjacency[from_index].size();
+        for (to_offset=0; to_offset<branch_size; to_offset++)
+        {
+            to_index = file_graph->adjacency[from_index][to_offset];
+            weight = file_graph->weight[from_index][to_offset];
+            edge_distribution[current_index] = weight;
+            neg_distribution[to_index] += weight;
+        }
+    }
+    this->edge_sampler.build(edge_distribution, 1.0);
+    this->negative_sampler.build(neg_distribution, 0.75);
+    std::cout << "\tdone" << std::endl;
+}
+
+long EdgeSampler::draw_an_edge() {
+    double rand_prob = random_range(0, 1);
+    long rand_index = random_range(0, this->edge_size);
+
+    if (rand_prob < this->edge_sampler.alias_probability[rand_index])
+        return rand_index;
+    else
+        return this->edge_sampler.alias_position[rand_index];
+}
+
+long EdgeSampler::draw_a_negative() {
+    double rand_prob = random_range(0, 1);
+    long rand_index = random_range(0, this->vertex_size);
+
+    if (rand_prob < this->negative_sampler.alias_probability[rand_index])
+        return rand_index;
+    else
+        return this->negative_sampler.alias_position[rand_index];
+}
+
